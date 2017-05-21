d1252406920f353cda6a6feafbe77ba65b200559
Andrey Litvinchuk <andrey.litvinchuk@yandex.ru>
2017-05-22 00:40:12 +0300

    player's factory bug

--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :player do
-    name "PlayerName-#{rand(9999999)}"
-    vk_id "#{rand(9999999)}"
-    level rand(20)
+    name { "PlayerName-#{rand(999999)}" }
+    vk_id { "#{rand(999999)}" }
+    level { rand(20) }
     last_visit "2017-05-21 10:30:27"
     sequence(:paid, &:even?)
   end
