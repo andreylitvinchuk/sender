796ea53c77def7df3727e068ded3a1464944a7ec
Andrey Litvinchuk <andrey.litvinchuk@yandex.ru>
2017-05-21 23:20:00 +0300

    tests for player

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'self.prop_names' do
+    subject { Player }
+    it 'exist' do
+      expect(subject).to respond_to(:prop_names)
+    end
+    it "shouldn't contain :id" do
+      expect(subject.prop_names).not_to include('id')
+    end
+    it "shouldn't contain :updated_at" do
+      expect(subject.prop_names).not_to include('updated_at')
+    end
+    it "shouldn't contain :created_at" do
+      expect(subject.prop_names).not_to include('updated_at')
+    end
+    it 'is array' do
+      expect(subject.prop_names.class).to eq Array
+    end
+  end
+
+
+
+
 end
