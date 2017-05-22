a7e1866e9e0caeb5993eb335baf2da9ea1d60dba
Andrey Litvinchuk <andrey.litvinchuk@yandex.ru>
2017-05-22 14:18:21 +0300

    add sidekiq monitor route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
 
   resources :players
@@ -7,4 +8,6 @@ Rails.application.routes.draw do
   root 'dashboard#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  mount Sidekiq::Web => '/sidekiq'
 end
