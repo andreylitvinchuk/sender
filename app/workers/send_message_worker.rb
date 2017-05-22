9275e4028c642de99c50f32d19a890738665778b
Andrey Litvinchuk <andrey.litvinchuk@yandex.ru>
2017-05-22 14:41:16 +0300

    set retry option for worker send message

--- app/workers/send_message_worker.rb
+++ app/workers/send_message_worker.rb
@@ -1,6 +1,6 @@
 class SendMessageWorker
   include Sidekiq::Worker
-  sidekiq_options queue: 'default'
+  sidekiq_options :retry => 5
 
   def perform(recipient_id)
     recipient = Recipient.find(recipient_id)
